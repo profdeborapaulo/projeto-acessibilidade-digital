9882bcb99442ac1accb6a043f52ce132561b5a28
Leticia Lemos <leticiasilveira189@gmail.com>
2024-09-20 11:06:55 -0300

    .

--- rhinny-ad/rhinny/firebase_js/authGuard.js
+++ rhinny-ad/rhinny/firebase_js/authGuard.js
@@ -1,15 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAK1Y4DAftt63mVx3tngghMWXYDfg5zzTo",
-    authDomain: "rhinny-319e6.firebaseapp.com",
-    projectId: "rhinny-319e6",
-    storageBucket: "rhinny-319e6.appspot.com",
-    messagingSenderId: "860506842460",
-    appId: "1:860506842460:web:e3176d1e88d16664590d99",
-    measurementId: "G-KKE67JZ0M6"
-}
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-
 //identifica o usuario a partir do estado de autenticação 
 firebase.auth().onAuthStateChanged((user) => {
     if(!user){
